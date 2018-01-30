eafb07abf1ad982c84396816a51952eb01fb7ea9
Patrick Dill <jamespatrickdill@gmail.com>
2018-01-30 15:51:54 -0500

    better help command

--- outlet/__init__.py
+++ outlet/__init__.py
@@ -28,7 +28,7 @@ from outlet.plugin import Plugin
 from outlet.background import run_every
 from outlet.bot import DiscordBot
 
-__version__ = (0, 4, 0)
+__version__ = (0, 4, 1)
 
 version_string = "{}.{}r{}".format(*__version__)
 
