792e509ab1ccdeee0feed8ce7e9a25cae6f372b9
Patrick Dill <jamespatrickdill@gmail.com>
2018-02-10 19:07:30 -0500

    fixed bug where background tasks wouldnt be destroyed when bot reconnects

--- outlet/plugin.py
+++ outlet/plugin.py
@@ -137,7 +137,7 @@ class Plugin(object):
 
         self.log.info("starting background tasks")
         for bg_task in self.bg_tasks:
-            self.bg_tasks.append(
+            self.running_bg_tasks.append(
                 self.create_task(bg_task(self.bot.loop))  # create the task, use the bot's event loop
             )
 
